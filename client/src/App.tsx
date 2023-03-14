b6b075f98e3ee8c6c42b31af4a27fa5cbb5f5cbb
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-14 16:01:22 +0200

    requerst test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,10 @@ import axios from "axios";
 
 function App() {
 
-    axios.get('  https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=Q3hOcpgVhyEZxQzucxCRqlNBa59SRhxX').then(data => console.log('articles', data));
-    axios.get('https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=Q3hOcpgVhyEZxQzucxCRqlNBa59SRhxX').then(data => console.log('most popular', data))
-    axios.get('https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=Q3hOcpgVhyEZxQzucxCRqlNBa59SRhxX').then(data => console.log('movie reviews', data))
-    axios.get('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=Q3hOcpgVhyEZxQzucxCRqlNBa59SRhxX').then(data => console.log('top stories', data))
+    axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${import.meta.env.API_KEY}`).then(data => console.log('articles', data));
+    axios.get(`https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=${import.meta.env.API_KEY}`).then(data => console.log('most popular', data))
+    axios.get(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=${import.meta.env.API_KEY}`).then(data => console.log('movie reviews', data))
+    axios.get(`https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${import.meta.env.API_KEY}`).then(data => console.log('top stories', data))
 
   return (
     <div className="App">
