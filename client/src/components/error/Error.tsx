42f4bd70de22ad78998e11f4a40a53bff8bed2e5
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-19 12:26:21 +0200

    error message

---
+++ client/src/components/error/Error.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {MotionContainer} from "@/components/motionContainer/MotionContainer";
+
+const Error = () => {
+    return (
+        <MotionContainer>
+            <div className="flex items-center justify-center flex-wrap">
+                <h2 className="text-4xl font-extrabold dark:text-white">Oops something went wrong...</h2>
+                <img className="my-2 w-full lg:w-1/2" src="/src/assets/err.png" alt="err image"/>
+            </div>
+        </MotionContainer>
+    );
+};
+
+export default Error;
