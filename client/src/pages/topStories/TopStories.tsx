109ca7eccd9bf354f219fbd3404fcd26ae5546a3
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-21 14:58:20 +0200

    res

--- client/src/pages/topStories/TopStories.tsx
+++ client/src/pages/topStories/TopStories.tsx
@@ -1,13 +1,26 @@
 import { MotionContainer } from "@/components/motionContainer/MotionContainer";
 import axios from "axios";
-
-const API_KEY = import.meta.env.VITE_API_KEY;
-
-axios
-  .get(`https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${API_KEY}`)
-  .then((data) => console.log("top stories", data));
+import { useDataFetch } from "@/hooks/useDataFetch";
+import { ITopStoriesResponse } from "@/types/nytimes";
+import Loader from "@/components/loader/Loader";
+import Error from "@/components/error/Error";
 
 const TopStories = () => {
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const URL = `https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${API_KEY}`;
+
+  const { status, hookRes } = useDataFetch<ITopStoriesResponse>(
+    URL,
+    "topStories"
+  );
+
+  if (status === "loading") return <Loader />;
+  if (status === "error") return <Error />;
+
+  const stories = hookRes?.results;
+
+  console.log(stories);
+
   return (
     <MotionContainer>
       <h1>TopStories</h1>
