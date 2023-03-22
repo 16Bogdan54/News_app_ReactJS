217eb376af04d35ceb7a9e4722bda65e06b649b1
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-22 11:02:18 +0200

    navlink active

--- client/src/components/navbar/navLinks/NavLinks.tsx
+++ client/src/components/navbar/navLinks/NavLinks.tsx
@@ -1,32 +1,37 @@
-import {NavLink} from "react-router-dom";
-import {ReactNode} from "react";
-import style from '@/components/navbar/styles/style.module.css'
+import { NavLink } from "react-router-dom";
+import { ReactNode } from "react";
+import style from "@/components/navbar/styles/style.module.css";
 
 type LinkProps = {
-    to: string,
-    children: ReactNode
-}
+  to: string;
+  children: ReactNode;
+};
 
 // `${({isActive}) => isActive ? "bg-blue-700" : "bg-white" }
 
-const CustomNavlink = ({to, children}:LinkProps) => {
-    return (
-        <li>
-            <NavLink to={to} className={`${style.nav_link}`}>
-                {children}
-            </NavLink>
-        </li>
-    );
+const CustomNavlink = ({ to, children }: LinkProps) => {
+  return (
+    <li>
+      <NavLink
+        to={to}
+        className={({ isActive }) =>
+          isActive ? style.nav_link_active : style.nav_link
+        }
+        end
+      >
+        {children}
+      </NavLink>
+    </li>
+  );
 };
 
 export const NavLinks = () => {
-    return (
-        <ul className={style.nav_links}>
-            <CustomNavlink to="/">Articles</CustomNavlink>
-            <CustomNavlink to="/most-popular">Most Popular</CustomNavlink>
-            <CustomNavlink to="/movie-reviews">Movie Reviews</CustomNavlink>
-            <CustomNavlink to="/top-stories">Top Stories</CustomNavlink>
-        </ul>
-    )
-}
-
+  return (
+    <ul className={style.nav_links}>
+      <CustomNavlink to="/">Articles</CustomNavlink>
+      <CustomNavlink to="/most-popular">Most Popular</CustomNavlink>
+      <CustomNavlink to="/movie-reviews">Movie Reviews</CustomNavlink>
+      <CustomNavlink to="/top-stories">Top Stories</CustomNavlink>
+    </ul>
+  );
+};
