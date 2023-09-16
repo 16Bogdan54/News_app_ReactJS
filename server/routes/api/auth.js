ad8fd1655536e173159e6d02fefee248d8e70031
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-09-16 20:25:23 +0300

    auth routes added

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,2 +1,37 @@
 const express = require("express");
-const { validationBody, authenticate } = require("../middlewares");
+const AuthController = require("../../controllers/auth_controller");
+const { validationBody, authenticate } = require("../../middlewares");
+const { userSchemas } = require("../../schemas/user");
+const router = express.Router();
+
+router.post(
+  "/signup",
+  validationBody(userSchemas.signupSchema),
+  AuthController.registration
+);
+
+router.post(
+  "/login",
+  validationBody(userSchemas.loginSchema),
+  AuthController.login
+);
+
+router.post(
+  "/token",
+  validationBody(userSchemas.refreshTokenSchema),
+  AuthController.refresh
+);
+
+router.get("/current", authenticate, AuthController.getCurrent);
+router.get("/logout", authenticate, AuthController.logout);
+
+router.put(
+  "/update",
+  validationBody(userSchemas.updateUserSchema),
+  AuthController.updateUser
+);
+
+router.get("/verify/:verificationToken", AuthController.verify);
+router.post("/verify", AuthController.reverify);
+
+module.exports = router;
